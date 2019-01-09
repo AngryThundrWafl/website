4051128cfac47916276bc1c176b8382c00d5aedc
AngryThundrWafl <pinabrian2015@gmail.com>
2019-01-09 00:57:46 -0700

    Refactored

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import {HashRouter,Route,Link,Switch} from 'react-router-dom';
 import About from './About.js';
-//import App from '../App.js';
 import Contact from './Contact.js';
 import Home from './Home.js';
 import Portfolio from './Portfolio.js';
 import Skills from './Skills.js';
 
 export default() => (
-  //<Route path='/' component={App}>
-    //<IndexRoute component={Home} />
-    //<Route path='About' component={About} />
-    //<Route path='Contact' component={Contact} />
-    //<Route path='Portfolio' component={Portfolio} />
-    //<Route path='Skills' component={Skills} />
-    //<Route path='*' component={Home} />
-  //</Route>
   <HashRouter>
     <Switch>
       <Route exact path='/About' component={About} />
--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import './index.css';
-//import App from './App';
-//import {Router, browserHistory} from 'react-router';
-//import Routes from './Routes';
-import Routes from './views/';
+import Routes from './Pages';
 import * as serviceWorker from './serviceWorker';
 
-//ReactDOM.render(<App />, document.getElementById('root'));
-
-//Router version of this application
-//ReactDOM.render(
-  //<Router history={browserHistory} routes={Routes} />,
-  //document.querySelector('#root')
-//);
-
+//renders pages with hashrouter
 ReactDOM.render(<Routes/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
